c2382dd556859d7a221e100ad17a0dfe9cbc7532
Nguyen Viet Quyen <jrnguyen14@gmail.com>
2025-08-09 16:21:43 +0700

    feat: config kafka

--- notification-service/src/main/java/com/devteria/notification/controller/EmailController.java
+++ notification-service/src/main/java/com/devteria/notification/controller/EmailController.java
@@ -1,16 +1,19 @@
 package com.devteria.notification.controller;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.devteria.notification.dto.ApiResponse;
 import com.devteria.notification.dto.request.SendEmailRequest;
 import com.devteria.notification.dto.response.EmailResponse;
 import com.devteria.notification.service.EmailService;
+
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,9 +23,14 @@ public class EmailController {
     EmailService emailService;
 
     @PostMapping("/email/send")
-    ApiResponse<EmailResponse> sendEmail(@RequestBody SendEmailRequest request){
+    ApiResponse<EmailResponse> sendEmail(@RequestBody SendEmailRequest request) {
         return ApiResponse.<EmailResponse>builder()
                 .result(emailService.sendEmail(request))
                 .build();
+    }
+
+    @KafkaListener(topics = "onboard-successful")
+    public void listen(String message) {
+        log.info("Received message: {}", message);
     }
 }
